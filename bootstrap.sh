5177ede4bd8fd6ae6dce28493705db3ccfc10994
Jamie Macey <jamie.git@tracefunc.com>
2021-12-10 21:24:34 -0800

    exclude encrypted files on initial bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,7 +22,9 @@ else
 fi
 
 ## Then chezmoi init from that checkout.
+## Codespaces ofc does not have private encryption keys, and does not run interactive,
+## so we should avoid attempting to decrypt files on initial bootstrap.
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir"
+exec "$chezmoi" init --apply "--source=$script_dir" --exclude=encrypted
